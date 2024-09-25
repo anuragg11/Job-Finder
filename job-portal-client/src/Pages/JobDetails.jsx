19619a4494c24f6ccc6a424a0deb76ed4f61a5e9
anuragg11 <129952211+anuragg11@users.noreply.github.com>
2024-09-25 17:06:58 +0530

    Update JobDetails.jsx

--- job-portal-client/src/Pages/JobDetails.jsx
+++ job-portal-client/src/Pages/JobDetails.jsx
@@ -11,7 +11,7 @@ const JobDetails = () => {
   const {id} = useParams();
   const [job,setJob] = useState([]);
   useEffect(()=>{
-   fetch(`http://localhost:5000/all-jobs/${id}`)
+   fetch(`https://job-finder-backend-etod.onrender.com/all-jobs/${id}`)
    .then(res => res.json())
    .then(data => setJob(data))
   },[]);
